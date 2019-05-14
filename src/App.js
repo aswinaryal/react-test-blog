06719ce04cd5883e61569bc1db1116223c37fcb2
AjayDhakal <aazaydhakal@gmail.com>
2019-05-14 20:42:16 +0545

    axios installed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './layout/header';
 import Navbar from './layout/navbar';
+import Body from './layout/body';
+import Footer from './layout/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar title = "React Blog" />
+      <Body />
+      <Footer />
     </div>
   );
 }
